bd4cd39428230aa18e657dacb0b671c89a8a2eca
martamonelli <marta.monelli@ipmu.jp>
2026-07-08 06:55:05 +0200

    separated functions

--- brahmap/lbsim/lbsim_GLS.py
+++ brahmap/lbsim/lbsim_GLS.py
@@ -9,7 +9,7 @@ from ..base import DTypeNoiseCov
 
 from ..core import GLSParameters, GLSResult, compute_GLS_maps_from_PTS
 
-from ..lbsim import LBSimProcessTimeSamples, DTypeLBSNoiseCov
+from ..lbsim import LBSimProcessTimeSamples, LBSimProcessTimeSamplesInpainting, LBSimProcessTimeSamplesZeroPadding, DTypeLBSNoiseCov
 
 from ..math import DTypeFloat
 
@@ -86,84 +86,54 @@ class LBSimGLSResult(GLSResult):
 
 def LBSim_compute_GLS_maps(
     nside: int,
-    observations: lbs.Observation | List[lbs.Observation],
-    pointings: npt.NDArray[np.number] | List[npt.NDArray[np.number]] | None = None,
-    hwp: lbs.HWP | None = None,
-    components: str | List[str] = "tod",
-    pointings_flag: npt.NDArray[np.bool_] | None = None,
-    inv_noise_cov_operator: DTypeNoiseCov | DTypeLBSNoiseCov | None = None,
+    observations: Union[lbs.Observation, List[lbs.Observation]],
+    pointings: Union[np.ndarray, List[np.ndarray], None] = None,
+    hwp: Optional[lbs.HWP] = None,
+    components: Union[str, List[str]] = "tod",
+    pointings_flag: Optional[np.ndarray] = None,
+    inv_noise_cov_operator: Union[DTypeNoiseCov, DTypeLBSNoiseCov, None] = None,
     threshold: float = 1.0e-5,
-    dtype_float: DTypeFloat = np.float64,
+    dtype_float: Optional[DTypeFloat] = None,
     LBSim_gls_parameters: LBSimGLSParameters = LBSimGLSParameters(),
-    x0: npt.NDArray[np.number] | None = None,
-    inpainting_params: List[int] | None = None,
-    zero_padding_len: int | None = None,
-) -> LBSimGLSResult | tuple[LBSimProcessTimeSamples, LBSimGLSResult]:
-    """Computes the Generalized Least Squares (GLS) maps from
-    `litebird_sim` observations.
+    x0: Union[np.ndarray, None] = None,
+) -> Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]:
+    """_summary_
 
     Parameters
     ----------
     nside : int
-        The HEALPix $N_{side}$ resolution parameter defining the number of pixels
-    observations : lbs.Observation | List[lbs.Observation]
-        An instance of the `Observation` class or a list of the same
-    pointings : npt.NDArray[np.number] | List[npt.NDArray[np.number]] | None, optional
-        Array of detector pointing indices mapping time samples to observed sky pixels,
-        by default `None`
-    hwp : lbs.HWP | None, optional
-        The Half-Wave Plate (HWP) angles or configuration, by default `None`
-    components : str | List[str], optional
-        A string or list defining the TOD components to be used for map-making, by
-        default `"tod"`
-    pointings_flag : npt.NDArray[np.bool_] | None, optional
-        Boolean array indicating valid pointing samples, by default `None`.
-        The `True` value indicates a valid pointing, and the `False`
-        value indicates a bad pointing. If set as `None`, all the
-        pointings are considered valid
-    inv_noise_cov_operator : DTypeNoiseCov | DTypeLBSNoiseCov | None, optional
-        The inverse noise covariance linear operator ($N^{-1}$), by default `None`
+        _description_
+    observations : Union[lbs.Observation, List[lbs.Observation]]
+        _description_
+    pointings : Union[np.ndarray, List[np.ndarray], None], optional
+        _description_, by default None
+    hwp : Optional[lbs.HWP], optional
+        _description_, by default None
+    components : Union[str, List[str]], optional
+        _description_, by default "tod"
+    pointings_flag : Optional[np.ndarray], optional
+        _description_, by default None
+    inv_noise_cov_operator : Union[DTypeNoiseCov, DTypeLBSNoiseCov, None], optional
+        _description_, by default None
     threshold : float, optional
-        The condition number threshold used to flag degenerate or
-        under-sampled pixels, by default `1.0e-5`
-    dtype_float : DTypeFloat, optional
-        The data type to use for floating point arrays, by default
-        `np.float64`
+        _description_, by default 1.0e-5
+    dtype_float : Optional[DTypeFloat], optional
+        _description_, by default None
     LBSim_gls_parameters : LBSimGLSParameters, optional
-        The parameter configuration dictating the map-making behavior, by
-        default `LBSimGLSParameters()`
-    x0 : npt.NDArray[np.number] | None, optional
-        Initial guess for the GLS solution in the form of interleaved
-        maps (e.g. $[I_1, Q_1, U_1, I_2, Q_2, U_2, \\dots]$), by default `None`
-    inpainting_params: List[int] | None = None,
-        A list of integers specifying the method parameters for inpainting, by default 'None' (no inpainting).
-        It expects the following structure: [inpainting_len, samples_per_bin, trash_pix_per_chunk], where:
-        - inpainting_len: the number of inpainted samples per chunk,
-        - samples_per_bin: number of samples per bin for the inpaining algorithm,
-        - trash_pix_per_chunk: number of trash pixels where to project the inpainted samples per chunk.
-    zero_padding_len: int | None = None,
-        The length of the zero-padding per chunk.
+        _description_, by default LBSimGLSParameters()
+    x0 : np.ndarray, optional
+        Initial guess for the GLS solution in the form 
+        [I_1, Q_1, U_1, I_2, Q_2, U_2, ...], by default None
+
     Returns
     -------
-    LBSimGLSResult | tuple[LBSimProcessTimeSamples, LBSimGLSResult]
-        The dataclass containing the final output from the GLS map-maker,
-        optionally returning the processed samples container
+    Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]
+        _description_
     """
     if inv_noise_cov_operator is None:
         noise_weights = None
     else:
         noise_weights = inv_noise_cov_operator.diag
-
-    if inpainting_params is not None:
-        inpainting_len, samples_per_bin, trash_pix_per_chunk = inpainting_params
-        extra_samples = inpainting_len
-        assert all(x > 0 for x in inpainting_params), "all inpainting parameters must be positive"
-        if zero_padding_len is not None:
-            print("You can't inpaint and zero-pad!")
-            quit()
-    elif zero_padding_len is not None:
-        trash_pix_per_chunk = 1
-        extra_samples = zero_padding_len
 
     processed_samples = LBSimProcessTimeSamples(
         nside=nside,
@@ -176,9 +146,121 @@ def LBSim_compute_GLS_maps(
         output_coordinate_system=LBSim_gls_parameters.output_coordinate_system,
         threshold=threshold,
         dtype_float=dtype_float,
+    )
+
+    if isinstance(components, str):
+        components = [components]
+
+    if len(components) > 1:
+        lbs.mapmaking.destriper._sum_components_into_obs(
+            obs_list=processed_samples.obs_list,
+            target=components[0],
+            other_components=components[1:],
+            factor=1.0,
+        )
+
+    time_ordered_data = np.concatenate(
+        [getattr(obs, components[0]) for obs in processed_samples.obs_list], axis=None
+    )
+
+    gls_result = compute_GLS_maps_from_PTS(
+        processed_samples=processed_samples,
+        time_ordered_data=time_ordered_data,
+        inv_noise_cov_operator=inv_noise_cov_operator,
+        gls_parameters=LBSim_gls_parameters,
+        x0=x0,
+    )
+
+    gls_result = LBSimGLSResult(
+        nside=nside,
+        coordinate_system=LBSim_gls_parameters.output_coordinate_system,
+        **asdict(gls_result),
+    )
+
+    if LBSim_gls_parameters.return_processed_samples:
+        return processed_samples, gls_result
+    else:
+        del processed_samples
+        gc.collect()
+        return gls_result
+
+
+def LBSim_compute_GLS_maps_inpainting(
+    nside: int,
+    observations: Union[lbs.Observation, List[lbs.Observation]],
+    inpainting_len: int,
+    samples_per_bin: int,
+    trash_pix_per_chunk: int,
+    pointings: Union[np.ndarray, List[np.ndarray], None] = None,
+    hwp: Optional[lbs.HWP] = None,
+    components: Union[str, List[str]] = "tod",
+    pointings_flag: Optional[np.ndarray] = None,
+    inv_noise_cov_operator: Union[DTypeNoiseCov, DTypeLBSNoiseCov, None] = None,
+    threshold: float = 1.0e-5,
+    dtype_float: Optional[DTypeFloat] = None,
+    LBSim_gls_parameters: LBSimGLSParameters = LBSimGLSParameters(),
+    x0: Union[np.ndarray, None] = None,
+) -> Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]:
+    """_summary_
+
+    Parameters
+    ----------
+    nside : int
+        _description_
+    observations : Union[lbs.Observation, List[lbs.Observation]]
+        _description_
+    inpainting_len : int
+        The number of inpainted samples per chunk
+    samples_per_bin : int
+        Number of samples per bin for the inpaining algorithm
+    trash_pix_per_chunk : int
+        Number of trash pixels where to project the inpainted samples per chunk
+    pointings : Union[np.ndarray, List[np.ndarray], None], optional
+        _description_, by default None
+    hwp : Optional[lbs.HWP], optional
+        _description_, by default None
+    components : Union[str, List[str]], optional
+        _description_, by default "tod"
+    pointings_flag : Optional[np.ndarray], optional
+        _description_, by default None
+    inv_noise_cov_operator : Union[DTypeNoiseCov, DTypeLBSNoiseCov, None], optional
+        _description_, by default None
+    threshold : float, optional
+        _description_, by default 1.0e-5
+    dtype_float : Optional[DTypeFloat], optional
+        _description_, by default None
+    LBSim_gls_parameters : LBSimGLSParameters, optional
+        _description_, by default LBSimGLSParameters()
+    x0 : np.ndarray, optional
+        Initial guess for the GLS solution in the form 
+        [I_1, Q_1, U_1, I_2, Q_2, U_2, ...], by default None
+
+    Returns
+    -------
+    Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]
+        _description_
+    """
+
+    assert all(x > 0 for x in [inpainting_len, samples_per_bin, trash_pix_per_chunk]), "all inpainting parameters must be positive"
+
+    if inv_noise_cov_operator is None:
+        noise_weights = None
+    else:
+        noise_weights = inv_noise_cov_operator.diag
+
+    processed_samples = LBSimProcessTimeSamplesInpainting(
+        nside=nside,
+        observations=observations,
         inpainting_len=inpainting_len,
-        zero_padding_len=zero_padding_len,
         trash_pix_per_chunk=trash_pix_per_chunk,
+        pointings=pointings,
+        hwp=hwp,
+        pointings_flag=pointings_flag,
+        solver_type=LBSim_gls_parameters.solver_type,
+        noise_weights=noise_weights,
+        output_coordinate_system=LBSim_gls_parameters.output_coordinate_system,
+        threshold=threshold,
+        dtype_float=dtype_float,
     )
 
     if isinstance(components, str):
@@ -194,62 +276,38 @@ def LBSim_compute_GLS_maps(
 
     start = time.time()
 
-    if inpainting_len != None:
-        time_ordered_data = np.empty(processed_samples.nsamples)
+    time_ordered_data = np.empty(processed_samples.nsamples)
 
-        end_idx = 0
+    end_idx = 0
 
-        for obs in observations:
-            fknees_mhz = obs.fknee_mhz
-            fmins_hz = obs.fmin_hz
-            alphas = obs.alpha
-            nets_ukrts = obs.net_ukrts
-            sampling_rate_hz = obs.sampling_rate_hz
+    for obs in observations:
+        fknees_mhz = obs.fknee_mhz
+        fmins_hz = obs.fmin_hz
+        alphas = obs.alpha
+        nets_ukrts = obs.net_ukrts
+        sampling_rate_hz = obs.sampling_rate_hz
 
-            for det_idx in range(obs.n_detectors):
-                tod_temp = obs.tod[det_idx]
-                nsamp_temp = len(tod_temp)
+        for det_idx in range(obs.n_detectors):
+            tod_temp = obs.tod[det_idx]
+            nsamp_temp = len(tod_temp)
 
-                fknee_hz = fknees_mhz[det_idx]*1e-3
-                fmin_hz = fmins_hz[det_idx]
-                alpha = alphas[det_idx]
-                net_ukrts = nets_ukrts[det_idx]
+            fknee_hz = fknees_mhz[det_idx]*1e-3
+            fmin_hz = fmins_hz[det_idx]
+            alpha = alphas[det_idx]
+            net_ukrts = nets_ukrts[det_idx]
 
-                start_idx = end_idx
-                end_idx += nsamp_temp
-                time_ordered_data[start_idx:end_idx] = tod_temp
+            start_idx = end_idx
+            end_idx += nsamp_temp
+            time_ordered_data[start_idx:end_idx] = tod_temp
 
-                start_idx = end_idx
-                end_idx += inpainting_len
+            start_idx = end_idx
+            end_idx += inpainting_len
 
-                x_sol = inpainting_func(tod_temp, inpainting_len, net_ukrts, fknee_hz, alpha, fmin_hz, sampling_rate_hz, samples_per_bin)
+            x_sol = inpainting_func(tod_temp, inpainting_len, net_ukrts, fknee_hz, alpha, fmin_hz, sampling_rate_hz, samples_per_bin)
 
-                time_ordered_data[start_idx:end_idx] = x_sol
+            time_ordered_data[start_idx:end_idx] = x_sol
 
-                start_idx = end_idx
-    elif zero_padding_len != None:
-        time_ordered_data = np.empty(processed_samples.nsamples)
-
-        end_idx = 0
-
-        for obs in observations:
-            for det_idx in range(obs.n_detectors):
-                tod_temp = obs.tod[det_idx]
-                nsamp_temp = len(tod_temp)
-
-                start_idx = end_idx
-                end_idx += nsamp_temp
-                time_ordered_data[start_idx:end_idx] = tod_temp
-
-                start_idx = end_idx
-                end_idx += zero_padding_len
-                time_ordered_data[start_idx:end_idx] = 0
-
-                start_idx = end_idx
-    else:
-        time_ordered_data = np.concatenate(
-            [getattr(obs, components[0]) for obs in processed_samples.obs_list], axis=None
-        )
+            start_idx = end_idx
     
     print(f"preprocessing took {time.time()-start} seconds")
 
@@ -265,18 +323,149 @@ def LBSim_compute_GLS_maps(
 
     print(f"map-making took {time.time()-start} seconds")
 
-    if inpainting_len != None:
-        lbsim_gls_result.GLS_maps = gls_result.GLS_maps[:,:12*nside**2]
+    gls_result.GLS_maps = gls_result.GLS_maps[:,:12*nside**2]
 
-    lbsim_gls_result = LBSimGLSResult(
+    gls_result = LBSimGLSResult(
         nside=nside,
         coordinate_system=LBSim_gls_parameters.output_coordinate_system,
         **asdict(gls_result),
     )
 
     if LBSim_gls_parameters.return_processed_samples:
-        return processed_samples, lbsim_gls_result
+        return processed_samples, gls_result
     else:
         del processed_samples
         gc.collect()
-        return lbsim_gls_result
+        return gls_result
+
+
+def LBSim_compute_GLS_maps_zero_padding(
+    nside: int,
+    observations: Union[lbs.Observation, List[lbs.Observation]],
+    zero_padding_len: int,
+    pointings: Union[np.ndarray, List[np.ndarray], None] = None,
+    hwp: Optional[lbs.HWP] = None,
+    components: Union[str, List[str]] = "tod",
+    pointings_flag: Optional[np.ndarray] = None,
+    inv_noise_cov_operator: Union[DTypeNoiseCov, DTypeLBSNoiseCov, None] = None,
+    threshold: float = 1.0e-5,
+    dtype_float: Optional[DTypeFloat] = None,
+    LBSim_gls_parameters: LBSimGLSParameters = LBSimGLSParameters(),
+    x0: Union[np.ndarray, None] = None,
+) -> Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]:
+    """_summary_
+
+    Parameters
+    ----------
+    nside : int
+        _description_
+    observations : Union[lbs.Observation, List[lbs.Observation]]
+        _description_
+    zero_padding_len : int
+        The number of zeros padded per chunk
+    pointings : Union[np.ndarray, List[np.ndarray], None], optional
+        _description_, by default None
+    hwp : Optional[lbs.HWP], optional
+        _description_, by default None
+    components : Union[str, List[str]], optional
+        _description_, by default "tod"
+    pointings_flag : Optional[np.ndarray], optional
+        _description_, by default None
+    inv_noise_cov_operator : Union[DTypeNoiseCov, DTypeLBSNoiseCov, None], optional
+        _description_, by default None
+    threshold : float, optional
+        _description_, by default 1.0e-5
+    dtype_float : Optional[DTypeFloat], optional
+        _description_, by default None
+    LBSim_gls_parameters : LBSimGLSParameters, optional
+        _description_, by default LBSimGLSParameters()
+    x0 : np.ndarray, optional
+        Initial guess for the GLS solution in the form 
+        [I_1, Q_1, U_1, I_2, Q_2, U_2, ...], by default None
+
+    Returns
+    -------
+    Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]
+        _description_
+    """
+
+    assert zero_padding_len > 0, "zero_padding_len must be positive"
+
+    if inv_noise_cov_operator is None:
+        noise_weights = None
+    else:
+        noise_weights = inv_noise_cov_operator.diag
+
+    processed_samples = LBSimProcessTimeSamplesZeroPadding(
+        nside=nside,
+        observations=observations,
+        zero_padding_len=zero_padding_len,
+        pointings=pointings,
+        hwp=hwp,
+        pointings_flag=pointings_flag,
+        solver_type=LBSim_gls_parameters.solver_type,
+        noise_weights=noise_weights,
+        output_coordinate_system=LBSim_gls_parameters.output_coordinate_system,
+        threshold=threshold,
+        dtype_float=dtype_float,
+    )
+
+    if isinstance(components, str):
+        components = [components]
+
+    if len(components) > 1:
+        lbs.mapmaking.destriper._sum_components_into_obs(
+            obs_list=processed_samples.obs_list,
+            target=components[0],
+            other_components=components[1:],
+            factor=1.0,
+        )
+
+    time_ordered_data = np.empty(processed_samples.nsamples)
+
+    end_idx = 0
+
+    for obs in observations:
+        fknees_mhz = obs.fknee_mhz
+        fmins_hz = obs.fmin_hz
+        alphas = obs.alpha
+        nets_ukrts = obs.net_ukrts
+        sampling_rate_hz = obs.sampling_rate_hz
+
+        for det_idx in range(obs.n_detectors):
+            tod_temp = obs.tod[det_idx]
+            nsamp_temp = len(tod_temp)
+
+            start_idx = end_idx
+            end_idx += nsamp_temp
+            time_ordered_data[start_idx:end_idx] = tod_temp
+
+            start_idx = end_idx
+            end_idx += zero_padding_len
+
+            time_ordered_data[start_idx:end_idx] = 0
+
+            start_idx = end_idx
+
+    gls_result = compute_GLS_maps_from_PTS(
+        processed_samples=processed_samples,
+        time_ordered_data=time_ordered_data,
+        inv_noise_cov_operator=inv_noise_cov_operator,
+        gls_parameters=LBSim_gls_parameters,
+        x0=x0,
+    )
+
+    gls_result.GLS_maps = gls_result.GLS_maps[:,:12*nside**2]
+
+    gls_result = LBSimGLSResult(
+        nside=nside,
+        coordinate_system=LBSim_gls_parameters.output_coordinate_system,
+        **asdict(gls_result),
+    )
+
+    if LBSim_gls_parameters.return_processed_samples:
+        return processed_samples, gls_result
+    else:
+        del processed_samples
+        gc.collect()
+        return gls_result
